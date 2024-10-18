3282e462b9042aca9fefa78fa163985238e5711d
Kasnix <luc.kasnix@gmail.com>
2024-10-18 20:26:17 -0300

    chore: add Material UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
-import type { Metadata } from "next";
-import type { ReactNode } from "react";
-import "./globals.css";
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Roboto } from 'next/font/google';
+
+import { theme } from '$/styles/theme';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 export const metadata: Metadata = {
-  title: "Todo App",
-  description: "An application to manage todos",
+  title: 'Todo App',
+  description: 'An application to manage todos',
 };
 
 const RootLayout = ({
@@ -13,9 +25,14 @@ const RootLayout = ({
   children: ReactNode;
 }>) => {
   return (
-    <html lang="en">
-      <body>
-        {children}
+    <html lang='en'>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+
+import styles from './page.module.css';
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <h1>Todo App</h1>
+      <Button variant='contained'>Contained</Button>
     </div>
   );
 };
